34e4ae9306c6594f7bd051cbc5d4a49f356a720d
Micah Thompson <mjthomp95@gmail.com>
2019-11-23 01:35:18 -0500

    initial website

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function card() {
+    var background = document.getElementById('card-background');
+
+    var card = document.getElementById('card');
+    var cardClassList = card.classList;
+    cardClassList.add("shrink");
+    cardClassList.remove("layer1hover");
+    card.onclick = null;
+
+    var frontClassList = card.getElementsByClassName('front')[0].classList;
+    var backClassList = card.getElementsByClassName('back')[0].classList;
+    frontClassList.add("shrink-img");
+
+    setTimeout( function(){
+        background.remove();
+        frontClassList.add("fronthover");
+        backClassList.add("backhover");}, 5000 );
+
+    var slices = background.getElementsByClassName('slice');
+    var i;
+    for (i = 0; i < slices.length; i++) {
+        slices[i].classList.add("fold");
+    }
+}
